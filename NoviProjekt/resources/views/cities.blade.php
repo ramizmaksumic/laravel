bee1ac7c4bc1d55937309a6c6623a417e38474f7
Ramiz <ramiz.maksumic@gmail.com>
2025-06-23 20:51:51 +0200

    Ispravljen layout, bila greška na tailwind klasama. Domaci br.10

--- NoviProjekt/resources/views/cities.blade.php
+++ NoviProjekt/resources/views/cities.blade.php
@@ -7,7 +7,7 @@
 <div class="row flex grid-cols-4 justify-between pt-20 mx-auto w-300 gap-3 ">
     <h3 class="text-xl font-bold bg-blue-500 text-white px-4 py-2 rounded-xl"><a href="/add-cities">Unesi novi grad</a> <span>+</span></h3>
 </div>
-<div class="row flex grid-cols-4 justify-between pt-20 mx-auto w-300 gap-3 ">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-20 max-w-7xl mx-auto mb-8">
 
     @foreach ($cities as $citie )
 
@@ -15,7 +15,7 @@
         <img class="w-full h-48 object-cover rounded-xl" src="{{ asset('images/Mostar.jpg') }}" alt="">
         <div class="info flex justify-between mt-2 px-2">
             <h2 class="font-bold text-2xl py-2">{{ $citie->name }}</h2>
-            <p class="font-bold bg-yellow-300 px-2 py-1 rounded-xl">{{$citie->deg}} C</p>
+            <p class="font-bold bg-yellow-300 px-2 py-1 rounded-xl">{{$citie->deg}}°C</p>
 
         </div>
         <p class="px-2">{{$citie->description}}</p>
